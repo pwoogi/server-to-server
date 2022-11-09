296714e59e2738b9acdf3bdea96729d81888d313
pwoogi <chrishw1029@gmail.com>
2022-11-09 21:05:20 +0900

    RestTempalte 활용 (심화) - naver api 간단하게 연동

--- src/main/java/com/example/client/controller/ApiController.java
+++ src/main/java/com/example/client/controller/ApiController.java
@@ -3,9 +3,16 @@ package com.example.client.controller;
 import com.example.client.dto.Req;
 import com.example.client.dto.UserResponse;
 import com.example.client.service.RestTemplateService;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
 
 @RestController
 @RequestMapping("/api/client")
@@ -21,6 +28,39 @@ public class ApiController {
     @GetMapping("/hello")
     public Req<UserResponse> getHello(){
         return restTemplateService.genericExchange();
+    }
+
+    //https://openapi.naver.com/v1/search/local.json
+    // ?query=%EC%A3%BC%EC%8B%9D
+    // &display=10
+    // &start=1
+    // &sort=random
+    @GetMapping("/naver")
+    public String naver(){
+
+        URI uri = UriComponentsBuilder
+                .fromUriString("https://openapi.naver.com")
+                .path("/v1/search/local.json")
+                .queryParam("query", "갈비집")
+                .queryParam("display",10)
+                .queryParam("start",1)
+                .queryParam("sort", "random")
+                .encode(StandardCharsets.UTF_8)
+                .build()
+                .toUri();
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        RequestEntity<Void> req = RequestEntity
+                .get(uri)
+                .header("X-Naver-Client-Id", "")
+                .header("X-Naver-Client-Secret","")
+                .build();
+
+        ResponseEntity<String> result = restTemplate.exchange(req, String.class);
+
+        return result.getBody();
+
     }
 
 }
